ed87f637f6bcfda17734e520058b93f6fb56d731
Nils Sanderson <thebaldwan@gmail.com>
2018-08-09 14:24:59 +0100

    Update file structure and imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Import all styles here for now
-import "./styles.css";
-import "./colors.css";
+import "./styles/styles.css";
 
 import Button from './components/Button';
 
